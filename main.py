a9abc37c56bfd3352a84324b63d22b6de77dddbc
Federico Cacace <federico@fcrepresentaciones.com>
2022-04-14 16:17:20 -0300

    Ejercicio1

--- main.py
+++ main.py
@@ -14,5 +14,6 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
-print ('Hola')
-print ('hola2')
+print('Hola, soy Federico Cacace')
+print('Estoy empezando el curso de python')
+print('Espero aprender muchos')
